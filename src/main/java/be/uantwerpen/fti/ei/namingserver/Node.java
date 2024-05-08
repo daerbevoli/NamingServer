db0315d58b8bcb730a0bf9ba41e8552b3efc4252
Ayoub-UA <117929082+Ayoub-UA@users.noreply.github.com>
2024-05-08 15:37:47 +0200

    changed a logger to see if number of nodes is correct when required

--- src/main/java/be/uantwerpen/fti/ei/namingserver/Node.java
+++ src/main/java/be/uantwerpen/fti/ei/namingserver/Node.java
@@ -330,7 +330,7 @@ public class Node {
         logger.log(Level.INFO, "CurrentID:"+currentID+" receivedID:"+receivedHash);
         // Update current node's network parameters based on the received node's hash
         if (receivedHash == currentID) { // Received info is about itself
-            logger.log(Level.INFO,"Received own bootstrap, my ID: "+currentID);
+            logger.log(Level.INFO,"Received own bootstrap, my ID: "+currentID+"\nMy number of nodes="+numOfNodes);
             if(numOfNodes >1)
             {
                 logger.log(Level.INFO,"Condition met to start TCP connection");
