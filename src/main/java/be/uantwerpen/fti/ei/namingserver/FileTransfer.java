fa4f3894576e51c6413ebdcff325be9f44e31792
samroels <sam.roels@student.uantwerpen.be>
2024-05-22 17:30:55 +0200

    test replication new nodeoffile2

--- src/main/java/be/uantwerpen/fti/ei/namingserver/FileTransfer.java
+++ src/main/java/be/uantwerpen/fti/ei/namingserver/FileTransfer.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.spi.InetAddressResolver;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
