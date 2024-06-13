985eb9aeac33de84a9f119c1cc82bd4e22f161c0
samroels <sam.roels@student.uantwerpen.be>
2024-06-13 22:31:24 +0200

    Agents v5

--- src/main/java/be/uantwerpen/fti/ei/namingserver/SyncAgent.java
+++ src/main/java/be/uantwerpen/fti/ei/namingserver/SyncAgent.java
@@ -106,16 +106,8 @@ public class SyncAgent implements Runnable, Serializable {
         String purpose = "Requesting File Map";
         helpMethods.sendUnicast(purpose, nextNodeIP, "REQUEST_FILE_MAP", port);
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            Socket socket = serverSocket.accept();
-            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-            @SuppressWarnings("unchecked")
-            Map<String, Boolean> nextNodeFileMap = (Map<String, Boolean>) inputStream.readObject();
-            return nextNodeFileMap;
-        } catch (IOException | ClassNotFoundException e) {
-            logger.log(Level.SEVERE, "Error retrieving file map from next node", e);
-            return null;
-        }
+        return node.getNextFileMap();
+
     }
 
     // Method to notify the next node to synchronize
