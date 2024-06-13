c614315acd0b3096078ee4398ccf6fec0e26300c
samroels <sam.roels@student.uantwerpen.be>
2024-06-13 22:46:39 +0200

    commit for reset

--- src/main/java/be/uantwerpen/fti/ei/namingserver/FailureAgent.java
+++ src/main/java/be/uantwerpen/fti/ei/namingserver/FailureAgent.java
@@ -4,9 +4,8 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Map;
 import java.util.logging.Logger;
-import jade.core.Agent;
 
-public class FailureAgent extends Agent implements Runnable, Serializable {
+public class FailureAgent implements Runnable, Serializable {
 
     private static final Logger logger = Logger.getLogger(FailureAgent.class.getName());
 
@@ -21,4 +20,8 @@ public class FailureAgent extends Agent implements Runnable, Serializable {
     }
 
 
+    @Override
+    public void run() {
+
+    }
 }
