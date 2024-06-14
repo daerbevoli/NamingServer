a8b5fc8567968a0dce06504d1c1bd831f76a778f
sameer <sameer.baruwal@student.uantwerpen.be>
2024-06-14 12:42:28 +0200

    ft bug fix

--- src/main/java/be/uantwerpen/fti/ei/namingserver/Ports.java
+++ src/main/java/be/uantwerpen/fti/ei/namingserver/Ports.java
@@ -4,11 +4,12 @@ public interface Ports {
 
     int unicastPort = 8000;
 
-    int ftPort = 8100;
-
     int replPort = 8200;
 
     int logPort = 8300;
 
     int nnPort = 8400;
+
+    int ftPort = 8500;
+
 }
